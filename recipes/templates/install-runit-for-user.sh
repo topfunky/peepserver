3f052dfe67f5138b21f42e12ca17721c2d98155c
Geoffrey Grosenbach <boss@topfunky.com>
2008-11-02 12:02:58 -0800

    Notes and final link to activate the user service

--- recipes/templates/install-runit-for-user.sh
+++ recipes/templates/install-runit-for-user.sh
@@ -11,10 +11,13 @@
 # See also: http://www.sanityinc.com/articles/init-scripts-considered-harmful
 # And also: http://smarden.sunsite.dk/runit/install.html
 
+# Make global service directory for this user
 sudo mkdir -p /etc/sv/$USER
 
+# Make user-specific service directory
 mkdir ~/service
 
+# Copy run script for this user
 cat >/tmp/run <<EOT
 #!/bin/sh
 exec 2>&1 
@@ -22,3 +25,6 @@ exec chpst -u $USER /usr/local/bin/runsvdir $HOME/service 'log: ................
 EOT
 sudo mv /tmp/run /etc/sv/$USER/run
 sudo chmod +x /etc/sv/$USER/run
+
+# Activate service
+sudo ln -s /etc/sv/$USER /service/
